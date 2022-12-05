030e0202ac9846e841fa437215aeda3f68b47c1f
anmedina-arg <andres.medina.arg@gmail.com>
2022-12-05 13:08:45 -0300

    first commit

---
+++ src/home/01-Home.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledHome = styled.div`
+  background-color: #000;
+  color: #fff;
+`;
+export const Home = () => {
+  return <StyledHome>hola soy home</StyledHome>;
+};
