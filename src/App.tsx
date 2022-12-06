7d9b29d852a85fc97e2c6650fe5877caf6780bce
anmedina-arg <andres.medina.arg@gmail.com>
2022-12-06 11:01:42 -0300

    fixed references scroll and container

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import { Gallery } from "./pages/06-Gallery";
 import { ThePad } from "./pages/07-ThePad";
 import { Tr01 } from "./pages/01-Home";
 
+var scrolling = false;
+
 export default function App() {
   const [active, setActive] = useState(0);
   const scrollRefs = useRef<any[]>([]);
@@ -31,30 +33,68 @@ export default function App() {
     (_, i) => scrollRefs.current[i] ?? createRef()
   );
 
-  console.log(scrollRefs);
-
   navRefs.current = [...Array.from(Array(list.length).keys())].map(
     (_, i) => navRefs.current[i] ?? createRef()
   );
 
-  console.log(navRefs);
-
   const scrollTo = (index: number) => {
+    scrolling = true;
+
     scrollRefs.current[index].current.scrollIntoView({
       behavior: "smooth",
-      inline: "start",
+      inline: "center",
     });
 
     setActive(index);
-    navRefs.current[index].current.scrollIntoView({
-      behavior: "smooth",
-      inline: "start",
+
+    setTimeout(() => {
+      scrolling = false;
+      navRefs.current[index].current.scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+      });
+    }, 1000);
+  };
+
+  /**/
+
+  const scrollHandler = (e: any) => {
+    // if (e.target !== document) return;
+
+    // if (scrolling === true) return;
+
+    const scrollRefsElements = scrollRefs.current;
+
+    scrollRefsElements.forEach((el: any, i: number) => {
+      const rect = el.current.getBoundingClientRect();
+      console.log(el);
+      const elemTop = rect.left;
+      const elemBottom = rect.right;
+      console.log(el.current.getBoundingClientRect().left);
+      console.log(elemBottom);
+      const isVisible =
+        elemTop < window.innerWidth / 2 && elemBottom > window.innerWidth / 2;
+
+      if (isVisible) {
+        navRefs.current[i].current.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "center",
+        });
+        setActive(i);
+      }
     });
   };
 
   useEffect(() => {
+    window.addEventListener("scroll", scrollHandler, true);
+    return () => {
+      window.removeEventListener("scroll", scrollHandler, true);
+    };
+  }, []);
+
+  useEffect(() => {
     const scrollContainer = document.querySelector("nav");
-    console.log(scrollContainer);
     if (scrollContainer === null) return;
     scrollContainer.addEventListener("wheel", (evt) => {
       evt.preventDefault();
@@ -62,6 +102,53 @@ export default function App() {
       scrollContainer.scrollIntoView({ behavior: "smooth" });
     });
   }, []);
+
+  /**/
+
+  // const scrollHandler = (e: any) => {
+  //   // if (e.target !== document) return;
+
+  //   // if (scrolling === true) return;
+
+  //   const scrollRefsElements = scrollRefs.current;
+
+  //   scrollRefsElements.forEach((el, i) => {
+  //     const rect = el.current.getBoundingClientRect();
+  //     const elemRight = rect.right;
+  //     const elemLeft = rect.left;
+
+  //     console.log(rect);
+  //     console.log(elemRight);
+  //     console.log(elemLeft);
+
+  //     const isVisible =
+  //       elemRight < window.innerWidth / 2 && elemLeft > window.innerWidth / 2;
+
+  //     if (isVisible) {
+  //       navRefs.current[i].current.scrollIntoView({
+  //         behavior: "smooth",
+  //         block: "start",
+  //         inline: "center",
+  //       });
+  //       setActive(i);
+  //     }
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   const scrollContainer = document.querySelector("nav");
+  //   if (scrollContainer === null) return;
+  //   scrollContainer.addEventListener("wheel", (evt) => {
+  //     evt.preventDefault();
+  //     scrollContainer.scrollLeft += evt.deltaY;
+  //   });
+
+  //   document.addEventListener("whell", scrollHandler, true);
+
+  //   return () => {
+  //     document.removeEventListener("whell", scrollHandler, true);
+  //   };
+  // }, []);
 
   return (
     <div className="main">
@@ -84,7 +171,12 @@ export default function App() {
       </div>
       <nav id="gallery" className="contenido">
         {list.map((item, i) => (
-          <div id={`s-${i}`} ref={scrollRefs.current[i]} className="py-100">
+          <div
+            id={`s-${i}`}
+            ref={scrollRefs.current[i]}
+            onWheel={scrollHandler}
+            className="py-100"
+          >
             {item.component}
           </div>
         ))}
